eb35ea98a8800b94c584cef6e24aa323ed77b4a0
prashant p <prashantsys19@gmail.com>
2022-07-09 22:19:28 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 
 from flask import Flask, jsonify, request
-from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
-run_with_ngrok(app)
 
 @app.route('/')
 def index():
@@ -21,4 +19,4 @@ def addTwoNum():
     return jsonify(retJson)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
